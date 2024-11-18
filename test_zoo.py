999483a6bad8323ff59c653278bce44bc31866a0
praiya-ch <praiya.chai@mail.kmutt.ac.th>
2024-11-18 20:02:52 +0000

    Added the test cases

--- test_zoo.py
+++ test_zoo.py
@@ -1,14 +1,24 @@
 import unittest
-from zoo import Zoo
+from zooticketcalculator import Zoo
 
-class TestZoo(unittest.TestCase):
+class TestZooTicketCalculator(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
+    def test_negative_age(self):
+        self.assertIsNone(self.zoo.get_ticket_price(-5))
+
     def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
-    # Add your additional test cases here.
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+
+    def test_teen_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+
+    def test_senior_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
 
 if __name__ == '__main__':
     unittest.main()
--- zoo.py
+++ zoo.py
@@ -1,10 +1,17 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        if 0 <= age <= 12:
             return 50
-        elif 13 <= age < 20:
+        elif 13 <= age <= 20:
             return 100
-        elif 21 < age <= 60:
+        elif 21 <= age <= 60:
             return 150
-        elif age >= 60:
+        elif age > 60:
             return 100
+
+if __name__ == "__main__":
+    zoo = Zoo()
+    ages = [-5, 12, 20, 21, 61]
+    for age in ages:
+        price = zoo.get_ticket_price(age)
+        print(f"ticket price for age {age} = {price}")
